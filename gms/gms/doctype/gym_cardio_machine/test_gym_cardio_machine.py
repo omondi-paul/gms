3f3e50cd9d345628fd48f4538fd66985a5246fc5
omondi-paul <omondipaulok@gmail.com>
2024-09-23 09:02:28 +0300

    gms doctypes

---
+++ gms/gms/doctype/gym_cardio_machine/test_gym_cardio_machine.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Polito and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGymCardioMachine(FrappeTestCase):
+	pass
