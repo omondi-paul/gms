7b29cb15ed4e1c0dae57b24c751c757011610d84
omondi-paul <omondipaulok@gmail.com>
2025-01-11 12:48:06 +0300

    send invoice whatsapp

--- gms/services/whatsapp.py
+++ gms/services/whatsapp.py
@@ -5,28 +5,46 @@ from whatsapp_integration.service.rest import send_whatsapp_media, send_whatsapp
 
 def send_whatsapp_job(mobile_number, invoice_number):
     try:
-        send_invoice_whatsapp(mobile_number, invoice_number)
+        send_invoice_whatsapp( invoice_number)
     except Exception as e:
         frappe.log_error(f"Failed to send WhatsApp message for invoice {invoice_number} to {mobile_number}: {str(e)}", "WhatsApp Notification Error")
 
+
 @frappe.whitelist(allow_guest=True)
-def send_invoice_whatsapp(mobile_number, invoice_name):
-    whatsapp_url = frappe.get_single("Gym URL").url
-    member_name = frappe.db.get_value("Gym Member", {"mobile_number": mobile_number}, ['full_name'])
-    payment_type = frappe.db.get_value('Sales Invoice', invoice_name, 'custom_payment_type')
-    media_url = f"{whatsapp_url}/api/method/frappe.utils.print_format.download_pdf?doctype=Sales%20Invoice&name={invoice_name}&format=Sales%20Invoice&no_letterhead=0&letterhead=Invoice%20LH&settings=%7B%7D&_lang=en"
-    msg = (f"Hey there, {member_name}!\n\nThis is your sales invoice. "
-           f"Be sure to press 'Pay' to redirect to the payment link and complete your {payment_type}.\n\n"
-           f"Thank you for choosing our services and staying fit with us!")
-    
+def send_invoice_whatsapp(invoice_name):
     try:
+        doc = frappe.get_doc("Sales Invoice", invoice_name)
+        member = frappe.get_all("Gym Member", {"full_name": doc.customer}, ["mobile_number"])
+        if not member:
+            frappe.throw(f"Gym Member not found for customer: {doc.customer}")
+
+        mobile_number = member[0].get("mobile_number")
+        if not mobile_number:
+            frappe.throw(f"Mobile number not found for Gym Member: {doc.customer}")
+
+        BASE_URL = frappe.get_single("Gym URL").url
+        member_name = frappe.db.get_value("Gym Member", {"mobile_number": mobile_number}, "full_name")
+        payment_type = frappe.db.get_value("Sales Invoice", invoice_name, "custom_payment_type")
+
+        media_url = (f"{BASE_URL}/api/method/frappe.utils.print_format.download_pdf?"
+                     f"doctype=Sales%20Invoice&name={invoice_name}&format=Sales%20Invoice"
+                     f"&no_letterhead=0&letterhead=Invoice%20LH&settings=%7B%7D&_lang=en")
+
+
+        msg = (f"Hello, {member_name}!\n\n"
+               f"Here is your sales invoice for your recent transaction. "
+               f"Please review it and click the 'Pay' button to complete your {payment_type}.\n\n"
+               f"Thank you for choosing us to support your fitness journey! If you have any questions, feel free to contact us.")
         res = send_whatsapp_media(to_number=mobile_number, message=msg, media_url=media_url)
         if "error" in res:
             frappe.log_error(f"WhatsApp media send error: {res['error']}", "WhatsApp Media Send Error")
+            frappe.throw("Failed to send the invoice via WhatsApp. Please try again.")
+
         return res
     except Exception as e:
         frappe.log_error(f"Unexpected error in sending WhatsApp message: {str(e)}", "WhatsApp Media Send Error")
         return {"error": str(e)}
+
 
 
 @frappe.whitelist(allow_guest=True)
